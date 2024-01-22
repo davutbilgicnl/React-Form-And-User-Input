94e4ee6e3a1bf2e6b3537b2cd5e7f0a1888dd1f8
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-01-22 12:54:02 +0100

    Managing & Getting User Input via State & Generic Handlers

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Submitted!");
+    console.log(typeof credentials.email, "- email: " + credentials.email);
+    console.log(typeof credentials.password, "- password: " + credentials.password);
+    console.log(typeof credentials, credentials);
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
+  //more generic versetile
+  function handleCredentialsChange(identifier, value) {
+    setCredentials((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
   }
 
   return (
@@ -11,12 +35,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={credentials.email}
+            onChange={(event) => handleCredentialsChange("email", event.target.value)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={(event) => handleCredentialsChange("password", event.target.value)}
+          />
         </div>
       </div>
 
